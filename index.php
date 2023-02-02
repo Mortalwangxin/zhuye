1e6278e1eb8a3067612bb844fcca6efd57cad0c1
Mortalwangxin <75226652+Mortalwangxin@users.noreply.github.com>
2023-02-02 13:34:04 +0800

    忘心个人主页

---
+++ index.php
@@ -0,0 +1,120 @@
+
+<!doctype html>
+<html>
+<head>
+            <!--移动适配-->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+        <!--UC强制全屏-->
+        <meta name="full-screen" content="yes">
+        <!--UC应用模式-->
+        <meta name="browsermode" content="application">
+        <!--QQ强制全屏-->
+        <meta name="x5-fullscreen" content="true">
+        <!--QQ应用模式-->
+        <meta name="x5-page-mode" content="app">
+        <!--删除默认的苹果工具栏和菜单栏-->
+        <meta content="yes" name="apple-mobile-web-app-capable">
+        <!--Cache-Control头域-->
+        <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+        <meta name="description" content="忘心 Home">
+        <meta name="keywords" content="忘心,忘心个人主页">
+        <meta name="author" content="忘心">
+        <title>忘心</title>
+        <link rel="icon" href="/favicon.ico">
+      <link rel="stylesheet" href="/css/mian.css">
+      <link rel="stylesheet" href="/css/iconfont.css">
+<body>
+  
+    <aside class="joe_aside">
+  <section class="joe_aside__item author">
+    <img width="100%" height="120" class="image lazyloaded" src="https://img.wxword.cn/2022/02/25/589767fe66ae8.jpg" >
+    <div class="user">
+      <img width="75" height="75" class="avatar ls-is-cached lazyloaded" src="https://thirdqq.qlogo.cn/g?b=qq&amp;nk=951961505&amp;s=100" alt="博主头像">
+      <a class="link" href="https://qm.qq.com/cgi-bin/qm/qr?k=CIDCpVBTsBD-wrnZHFPFsbyJVpLFSJck&amp;noverify=0" target="_blank" rel="noopener noreferrer nofollow">忘心</a>	
+      <ul class="joe_header__slideout-count">
+   
+      <li class="item">
+        
+        <p >简介</p>
+        <p >欢迎来到忘心的个人主页，这里展示的是忘心的联系方式和发布文章的一些地址。</p>
+        <p>
+          忘心 —【听闻远方有她，便策马扬鞭万里！】
+        </p>
+      </li>
+    </ul>
+       
+      <!--技能-->
+      <div class="coun">
+      <div class="ite" title="CSS">
+        <span class="num">CSS</span>
+        <span>45%</span>
+      </div>
+        <div class="ite" title="HTML5">
+        <span class="num">HTML5</span>
+        <span>75%</span>
+      </div>
+        <div class="ite" title="Ps">
+        <span class="num">Ps</span>
+        <span>30%</span>
+      </div>
+      <div class="ite" title="Java">
+        <span class="num">Java</span>
+        <span>40%</span>
+      </div>
+    </div>
+      <div class="count">
+       <div  class="item" >
+       <a href="https://www.wxword.cn" ><button class="joe_load">博客</button> </a>        
+      </div> 
+        
+        
+        <div  class="item" >
+       <a href="https://dh.wxword.cn" ><button class="joe_load">导航</button> </a>        
+      </div> 
+        <div  class="item" >
+       <a href="https://github.com/Mortalwangxin" ><button class="joe_load">Github</button> </a>        
+      </div> 
+      <div  class="item" >
+       <a href="https://www.wxword.cn/wxlyq/api.php?qq=951961505" ><button class="joe_load">联系我</button> </a>        
+      </div> 
+    </div>
+    </div>
+    
+      
+
+         <div class="count">
+          
+       <div  class="item" >
+       <a href="https://www.wxword.cn/wxlyq/api.php?qq=951961505" ><i class='iconfont iconfont icon-qq'></i></a>        
+      </div> 
+        <div  class="item" >
+       <a href="https://github.com/Mortalwangxin" ><i class='iconfont iconfont icon-github'></i></a>        
+      </div> 
+        <div  class="item" >
+       <a href="https://gitee.com/wangxinqq" ><i class='iconfont iconfont icon-mayun'></i></a>        
+      </div> 
+      <div  class="item" >
+       <a href="mailto:wx-kj@qq.com" title="email" ><i class='iconfont iconfont iconfont icon-email'></i>
+        </a>        
+      </div> 
+           
+    </div>
+
+      
+             
+    <div  class="item" >
+      
+       <a class="footer" href="https://qm.qq.com/cgi-bin/qm/qr?k=CIDCpVBTsBD-wrnZHFPFsbyJVpLFSJck&amp;noverify=0" target="_blank" rel="noopener noreferrer nofollow">Copyright&nbsp;©&nbsp;2023&nbsp;&nbsp;忘心</a>
+       <a class="footer" href="https://beian.miit.gov.cn" target="_blank" rel="noopener noreferrer nofollow">鲁ICP备2023000232号</a>
+         
+      </div> 
+      <audio controls autoplay="autoplay" loop="loop" hidden="true" class="joe_header__slideout-count"  style="width: 230px; heigh :80px" >
+<source src="https://s1.ananas.chaoxing.com/sv-w8/audio/86/3b/78/af5b0bb4515dc8ca49de83bf31d86260/audio.mp3?at_=1675255950558&ak_=ec86105f285d113b95520079719f951d&ad_=72d95d195194e3c34865c6ff63534c26" type="audio/mpeg"  />
+<embed height="50" width="100" src="https://s1.ananas.chaoxing.com/sv-w8/audio/86/3b/78/af5b0bb4515dc8ca49de83bf31d86260/audio.mp3?at_=1675255950558&ak_=ec86105f285d113b95520079719f951d&ad_=72d95d195194e3c34865c6ff63534c26">
+</audio>   
+      
+    </section>
+          
+  </aside>
+</body>
+</html>
